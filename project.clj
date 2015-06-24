ed37fc0b8db96a10faf59b682647ea9a86a66bba
Simon Monecke <SimonMonecke@gmail.com>
2015-06-24 15:21:01 +0200

    add license

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lambdacd-mongodb "0.1.0"
             :description "LambdaCD extension which lets you use a MongoDB to persist the state of pipelines"
             :url "https://github.com/SimonMonecke/lambdacd-mongodb"
+            :license {:name "The MIT License (MIT)"
+                      :url "http://opensource.org/licenses/MIT"}
             :dependencies [[lambdacd "0.4.0"]
                            [ring-server "0.3.1"]
                            [org.clojure/clojure "1.6.0"]
