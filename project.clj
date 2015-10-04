10379a943270a0e2357740862b4546f93a314045
Simon Monecke <SimonMonecke@gmail.com>
2015-10-04 18:22:18 +0200

    update to clojure 1.7

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                   :url "https://github.com/SimonMonecke/lambdacd-mongodb.git"}
             :dependencies [[lambdacd "0.5.3"]
                            [ring-server "0.3.1"]
-                           [org.clojure/clojure "1.6.0"]
+                           [org.clojure/clojure "1.7.0"]
                            [org.clojure/tools.logging "0.3.0"]
                            [org.slf4j/slf4j-api "1.7.5"]
                            [ch.qos.logback/logback-core "1.0.13"]
