919535480b7234e1c14e3b8aa0a4aa837e377a9a
Simon Monecke <SimonMonecke@gmail.com>
2016-01-29 19:18:02 +0100

    modify example step

--- example/example_pipeline/steps.clj
+++ example/example_pipeline/steps.clj
@@ -1,14 +1,25 @@
 (ns example-pipeline.steps
-  (:require [lambdacd.steps.shell :as shell]))
+  (:require [lambdacd.steps.shell :as shell]
+            [lambdacd.steps.support :refer [new-printer print-to-output printed-output]]))
 
 (defn some-step-that-does-nothing [& _]
   {:status :success})
 
+(defn write-n-times [ctx printer n text]
+  (if (= n 1)
+    (do
+      (print-to-output ctx printer text)
+      {:status :success})
+    (do
+      (Thread/sleep 1000)
+      (print-to-output ctx printer text)
+      (recur ctx printer (- n 1) text))))
+
+
 (defn some-step-that-echos-foo [_ ctx]
   (shell/bash ctx "/" "echo foo"))
 (defn some-step-that-echos-bar [_ ctx]
-  (Thread/sleep 10000)
-  (shell/bash ctx "/" "echo bar"))
+  (write-n-times ctx (new-printer) 20 "echo bar"))
 
 (defn some-failing-step [_ ctx]
   (shell/bash ctx "/" "echo \"i am going to fail now...\"" "exit 1"))
