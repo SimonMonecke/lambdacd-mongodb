7bc50df3694e8aaa01bd8f6e2d6f152c3a6de7fe
Martha Rohte <mrohte@thoughtworks.com>
2016-05-06 11:40:15 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,15 +5,15 @@
                       :url "http://opensource.org/licenses/MIT"}
             :scm {:name "git"
                   :url "https://github.com/SimonMonecke/lambdacd-mongodb.git"}
-            :dependencies [[lambdacd "0.8.0"]
-                           [ring-server "0.3.1"]
-                           [org.clojure/clojure "1.7.0"]
-                           [org.clojure/tools.logging "0.3.0"]
-                           [org.slf4j/slf4j-api "1.7.5"]
-                           [ch.qos.logback/logback-core "1.0.13"]
-                           [ch.qos.logback/logback-classic "1.0.13"]
+            :dependencies [[lambdacd "0.9.0"]
+                           [ring-server "0.4.0"]
+                           [org.clojure/clojure "1.8.0"]
+                           [org.clojure/tools.logging "0.3.1"]
+                           [org.slf4j/slf4j-api "1.7.21"]
+                           [ch.qos.logback/logback-core "1.1.7"]
+                           [ch.qos.logback/logback-classic "1.1.7"]
                            [com.novemberain/monger "3.0.2"]
-                           [com.rpl/specter "0.7.1"]]
+                           [com.rpl/specter "0.10.0"]]
             :test-paths ["test", "example"]
             :profiles {:uberjar {:aot :all}}
             :main example-pipeline.pipeline)
